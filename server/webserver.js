4ad5081c7f6685676613c6b72837caa7b08f7d4f
hubr64 <hubr.md@gmail.com>
2018-10-10 22:18:17 +0200

    AJout d'un webserver classique utilisant express sur des fichiers static (partie client)

---
+++ server/webserver.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8081;
+
+app.use(express.static('public'));
+
+var server = app.listen(port, function() {
+  console.log('myLaboWebServer est maintenant démarré sur le port ' + port);
+});
